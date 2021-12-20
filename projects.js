a339b0da5298fac323039731cb1f20e7d9646657
Helen Charbonnet <helendc@live.unc.edu>
2021-12-20 15:19:12 -0500

    updates + new page

---
+++ projects.js
@@ -0,0 +1,7 @@
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+ready(() => {
+    document.getElementById("#sushipro").addClass("shadow");
+})
