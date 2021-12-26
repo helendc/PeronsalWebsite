bf6cb563594c92a701b8e24886f3713eade31165
Helen Charbonnet <helendc@live.unc.edu>
2021-12-26 14:14:55 -0500

    last few changes

--- projects.js
+++
@@ -1,7 +0,0 @@
-var ready = (callback) => {
-    if (document.readyState != "loading") callback();
-    else document.addEventListener("DOMContentLoaded", callback);
-}
-ready(() => {
-    document.getElementById("#sushipro").addClass("shadow");
-})
